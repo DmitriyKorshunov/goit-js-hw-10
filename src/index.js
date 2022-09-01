4af632689e6352c977f62f62146ee830601d8634
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-09-01 17:57:43 +0300

    Находит только одну страну, список не выводит

--- src/index.js
+++ src/index.js
@@ -13,12 +13,17 @@ console.log(refs.searchBox);
 console.log(refs.countryList);
 console.log(refs.countryInfo);
 
+refs.searchBox.addEventListener(
+  'input',
+  debounce(onInputCountry, DEBOUNCE_DELAY)
+);
+
 //http делает запрос
 
 function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com/v3.1/name';
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
   return fetch(
-    `${BASE_URL}?fields=name,capital,population,flags,languages`
+    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
   ).then(res => {
     if (!res.ok) {
       throw new Error(res.status);
@@ -43,4 +48,51 @@ function countryCardTeemplate({ flags, name, capital, population, languages }) {
       )}</p>
     </div>
   `;
+}
+
+function countryListTempate({ flags, name }) {
+  return `<li class="country-list_item">
+  <img class="country-list_flags" src="${flags.svg}" alt="${name.official}" width="25">
+  <h2 class="country-list__name">${name.official}</h2>;
+  </li>
+  `;
+}
+
+function onInputCountry() {
+  const countryName = refs.searchBox.value;
+  if (countryName === '') {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(countryName)
+    .then(countrys => {
+      if (countrys.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+        return;
+      }
+
+      if (countrys.length <= 10) {
+        const listMarkup = countrys.map(country => countryListTempate(country));
+        refs.countryInfo.innerHTML = listMarkup.join('');
+        refs.countryList.innerHTML = '';
+      }
+
+      if ((countrys.length = 1)) {
+        const markup = countrys.map(country => countryCardTeemplate(country));
+        refs.countryInfo.innerHTML = markup.join('');
+        refs.countryList.innerHTML = '';
+      }
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+      refs.countryInfo.innerHTML = '';
+      refs.countryList.innerHTML = '';
+      return error;
+    });
 }

86a7259f16349611aa28885b7122889da005a904
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-09-01 20:30:31 +0300

    Работает без оформления css

--- src/index.js
+++ src/index.js
@@ -1,98 +1,52 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { refs } from './js/refselements';
+import { fetchCountries } from './js/fetchCountries';
+import { countryCardTeemplate, countryListTempate } from './js/teemplate';
 
 const DEBOUNCE_DELAY = 300;
-
-const refs = {
-  searchBox: document.getElementById('search-box'),
-  countryList: document.querySelector('ul.country-list'),
-  countryInfo: document.querySelector('div.country-info'),
-};
-console.log(refs.searchBox);
-console.log(refs.countryList);
-console.log(refs.countryInfo);
 
 refs.searchBox.addEventListener(
   'input',
   debounce(onInputCountry, DEBOUNCE_DELAY)
 );
 
-//http делает запрос
-
-function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com/v3.1/name/';
-  return fetch(
-    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
-  ).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json();
-  });
-}
-//Разметка карточки
-function countryCardTeemplate({ flags, name, capital, population, languages }) {
-  return `
-    <div class="country-info__container">
-      <div class="country-info__wrapper">
-        <img class="country-info__flags" src="${flags.svg}" alt="${
-    name.official
-  }" width="50" />
-        <h2 class="country-info__name">${name.official}</h2>
-      </div>
-      <p class="country-info__capital"><span class="country-info__weight">Capital:</span> ${capital}</p>
-      <p class="country-info__population"><span class="country-info__weight">Population:</span> ${population}</p>
-      <p class="country-info__languages"><span class="country-info__weight">Languages:</span> ${Object.values(
-        languages
-      )}</p>
-    </div>
-  `;
-}
-
-function countryListTempate({ flags, name }) {
-  return `<li class="country-list_item">
-  <img class="country-list_flags" src="${flags.svg}" alt="${name.official}" width="25">
-  <h2 class="country-list__name">${name.official}</h2>;
-  </li>
-  `;
-}
-
 function onInputCountry() {
   const countryName = refs.searchBox.value;
+  clearDataInput();
   if (countryName === '') {
-    refs.countryInfo.innerHTML = '';
-    refs.countryList.innerHTML = '';
     return;
   }
 
-  fetchCountries(countryName)
+  fetchCountries(countryName.trim())
     .then(countrys => {
+      console.log(countrys);
       if (countrys.length > 10) {
         Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
-        refs.countryInfo.innerHTML = '';
-        refs.countryList.innerHTML = '';
         return;
       }
 
       if (countrys.length <= 10) {
         const listMarkup = countrys.map(country => countryListTempate(country));
         refs.countryInfo.innerHTML = listMarkup.join('');
-        refs.countryList.innerHTML = '';
       }
 
-      if ((countrys.length = 1)) {
+      if (countrys.length === 1) {
         const markup = countrys.map(country => countryCardTeemplate(country));
         refs.countryInfo.innerHTML = markup.join('');
-        refs.countryList.innerHTML = '';
       }
     })
     .catch(error => {
       Notify.failure('Oops, there is no country with that name');
-      refs.countryInfo.innerHTML = '';
-      refs.countryList.innerHTML = '';
+
       return error;
     });
+}
+
+function clearDataInput() {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
 }
